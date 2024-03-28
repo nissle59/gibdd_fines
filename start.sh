7ec343c8eaa996dc5a04a02016bd8bcfe24297a1
Mikhaylov Nikita <nixncom@gmail.com>
2024-03-28 08:29:22 +0300

    Upd

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ pip install -r /var/www/html/requirements.txt > /dev/null 2>&1
 #rm `$(docker inspect --format="{{.LogPath}}" parser_gibdd_fines)`
 #chmod +x ./run_workers.sh
 #./run_workers.sh &
-uvicorn server:app --host 0.0.0.0 --port 5006 --workers 10
+uvicorn server:app --host 0.0.0.0 --port 5006 --workers 3
 tail -f /dev/null
